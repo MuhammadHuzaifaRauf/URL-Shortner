80d3a96718155505b20c7ac95d456dd99229081e
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-03-12 14:31:01 +0500

    Designing Home Page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
+import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Register from "./(auth)/register/page";
 import Login from "./(auth)/login/page";
+import EditPage from "./(dashboard)/EditPage/page";
 
 export default function page() {
   return (
     <>
+      <EditPage />
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
     </>
   );
 }
